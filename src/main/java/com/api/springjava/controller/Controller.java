3e9827af8754a87ef517dfe0415e0690e0c3dc38
Walberg Santos <fwalbergsantos@outlook.com>
2022-04-20 18:44:39 +0000

    Impementado CRUD

--- src/main/java/com/api/springjava/controller/Controller.java
+++ src/main/java/com/api/springjava/controller/Controller.java
@@ -1,12 +1,16 @@
 package com.api.springjava.controller;
 
+import java.util.List;
+
 import com.api.springjava.model.Pessoa;
 import com.api.springjava.repository.Repositorio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +26,63 @@ public class Controller {
     public Pessoa cadastrar(@RequestBody Pessoa obj) {
         return acao.save(obj);
     }
+
+    @GetMapping("/api")
+    public List<Pessoa> selecionar() {
+        return acao.findAll();
+    }
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo) {
+        return acao.findByCodigo(codigo);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj) {
+        return acao.save(obj);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo) {
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
+
+    @GetMapping("api/contador")
+    public long contador() {
+        return acao.count();
+    }
+
+    @GetMapping("api/ordenarNomes")
+    public List<Pessoa> ordenarNomes() {
+        return acao.findByOrderByNome();
+    }
+
+    @GetMapping("api/ordenarNomes2")
+    public List<Pessoa> ordenarNomes2() {
+        return acao.findByNomeOrderByIdadeDesc("Davi Barros dos Santos");
+    }
+
+    @GetMapping("api/filtro")
+    public List<Pessoa> selecionarPorTermo() {
+        return acao.findByNomeContaining("Maria");
+    }
+
+    @GetMapping("api/iniciaCom")
+    public List<Pessoa> iniciaCom() {
+        return acao.findByNomeStartsWith("D");
+    }
+
+    @GetMapping("api/terminaCom")
+    public List<Pessoa> terminaCom() {
+        return acao.findByNomeEndsWith("s");
+    }
+
+    
+
+
+
+    //##############################################################################
 
     @GetMapping("") // Marca o método como uma rota com requisição GET.
     public String mensagem() {
--- src/main/java/com/api/springjava/repository/Repositorio.java
+++ src/main/java/com/api/springjava/repository/Repositorio.java
@@ -1,5 +1,7 @@
 package com.api.springjava.repository;
 
+import java.util.List;
+
 import com.api.springjava.model.Pessoa;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,4 +10,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
     
+    List<Pessoa> findAll();
+
+    Pessoa findByCodigo(int codigo);
+
+    List<Pessoa> findByOrderByNome();
+
+    List<Pessoa> findByNomeOrderByIdadeDesc(String nome);
+
+    List<Pessoa> findByNomeContaining(String termo);
+
+    List<Pessoa> findByNomeStartsWith(String termo);
+
+    List<Pessoa> findByNomeEndsWith(String termo);
 }
